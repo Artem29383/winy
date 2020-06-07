1f15220fb17138a629e9c037cccde4431ff3109f
Artem29383 <54779368+Artem29383@users.noreply.github.com>
2020-06-07 16:09:44 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCErJJduKqG1ZyYqFmNKdBIfPsNFhp42sY',
-  authDomain: 'mini-twitter-ced4f.firebaseapp.com',
-  databaseURL: 'https://mini-twitter-ced4f.firebaseio.com',
-  projectId: 'mini-twitter-ced4f',
-  storageBucket: 'mini-twitter-ced4f.appspot.com',
-  messagingSenderId: '652955228797',
-  appId: '1:652955228797:web:7d13e50cc06249ca47af14',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
